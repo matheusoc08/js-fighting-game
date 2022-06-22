1ec5ba80d62dd905f3946b6a4cd32a017f49fe3a
Matheusoc <matheuso.c08@gmail.com>
2022-06-22 16:05:31 -0300

    Added a shop sprite at the background and using the update function with new properties to make a kind of animation with it

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -13,6 +13,16 @@ const background = new Sprite({
     imageSrc: './img/background.png'
 })
 
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 128
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
+
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const player = new Fighter({
@@ -77,6 +87,7 @@ function animate(){
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height);
     background.update();
+    shop.update();
     player.update();
     enemy.update();
 
